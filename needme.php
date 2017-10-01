d465a38db90711afd900ef6f690f984bafe45ebe
vishal-pandey <pandeyvishal25@yahoo.com>
2017-10-02 01:31:20 +0530

    changes to portfolio

---
+++ needme.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Vishal Pandey</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="menu.css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaina|Pacifico|Shrikhand|Permanent+Marker" rel="stylesheet">
+    <meta name="theme-color" content="pink">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="Unicode/UTF-8">
+    <meta name="description" content="vishal pandey is a budding web designer. This is portfolio of Vishal pandey">
+    <meta name="keywords" content="vishalpandey vishal pandey pandeyvishal vishalthewebdesigner web designer">
+    <link rel="icon" 
+      type="image/png" 
+      href="./myicon.png">
+  </head>
+<body>
+<?php include 'menu.txt'; ?>
+<br><br>
+
+<div class="col-12 container11">
+  <h2>There are the things</h2>
+  <div class="service">
+    <h3>Web Designing</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Web App Development</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>UI / UX</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Dashboard Designing</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Content Writing</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Portal Design</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>E-Commerce Solutions</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Customer Care Solution For Business</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Business Consultancy</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Domain Names</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Web/App Hosting Solutions</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Cloud Computing</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Cloud Based Data Back Up Solutions for business</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Database Support</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Android App Development</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>IOS App Development</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Windows App Development</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Mac OS App Development</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>IOT Based Solutions</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>IOT Based Remote Control System</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Realtime Info Broadcast Through IOT</h3>
+    <p></p>
+  </div>
+
+  <div class="service">
+    <h3>Can Any Thing Smart Trough IOT</h3>
+    <p></p>
+  </div>
+
+</div>
+<?php include './footer.txt'; ?>
+
+</body>
+</html>
+<style type="text/css">
+  body{
+    width: 100%;
+    height: auto;
+  }
+  .container11{
+    width: 100%;
+    background-color: rgba(0,0,0,0.7);
+  }
+  .container11 h2{
+    text-align: center;
+    color: white;
+  }
+  .service{
+    width: 24%;
+    height: 100px;
+    padding: 15px;
+    background-color: white;
+    border-radius: 5px;
+    float: left;
+    margin: 5px;
+    color: blue;
+  }
+  .service h3{
+    margin: 0;
+    text-align: center;
+  }
+
+@media only screen and (max-width: 768px) {
+  .service{
+    width: 100%;
+    
+    margin-left: 0px;
+    margin-right: 0px;
+  }
+}
+
+</style>
