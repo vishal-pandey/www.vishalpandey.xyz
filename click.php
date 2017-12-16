933f732bc8c4f3c1ed6d5437e6383a8dc27f211f
vishal-pandey <pandeyvishal64742@gmail.com>
2017-12-16 19:28:20 +0530

    click update

--- click.php
+++ click.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Cick</title>
-	<meta http-equiv="refresh" content="1">
+	<meta http-equiv="refresh" content="5">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
